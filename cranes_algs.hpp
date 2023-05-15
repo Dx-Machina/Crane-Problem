ba7725ccec5d78a67dde722da9a7cadaa4fe2fd5
Youssef H <123545124+Dx-Machina@users.noreply.github.com>
2023-05-15 02:13:50 -0700

    cranes_algs.hpp

--- cranes_algs.hpp
+++ cranes_algs.hpp
@@ -13,12 +13,13 @@
 #pragma once
 
 #include <cassert>
+
 #include <math.h>
 
 #include "cranes_types.hpp"
 
-namespace cranes {
-
+namespace cranes 
+{
 // Solve the crane unloading problem for the given grid, using an exhaustive
 // optimization algorithm.
 //
@@ -27,8 +28,8 @@ namespace cranes {
 // with an assertion.
 //
 // The grid must be non-empty.
-path crane_unloading_exhaustive(const grid& setting) {
-
+path crane_unloading_exhaustive(const grid& setting) 
+{
   // grid must be non-empty.
   assert(setting.rows() > 0);
   assert(setting.columns() > 0);
@@ -37,50 +38,133 @@ path crane_unloading_exhaustive(const grid& setting) {
   const size_t max_steps = setting.rows() + setting.columns() - 2;
   assert(max_steps < 64);
 
-  // TODO: implement the exhaustive search algorithm, then delete this
-  // comment.
   path best(setting);
-  for (size_t steps = 0; steps <= max_steps; steps++) {
+
+  for (size_t steps = 1; steps <= max_steps; ++steps) 
+  {
+    uint64_t Pos_Int = uint64_t(1) << steps;
+
+    for (uint64_t MyBit = 0; MyBit < Pos_Int; ++MyBit) 
+    {
+      path candidate(setting);
+      bool status = true;
+
+      for (size_t i = 0; i < steps; ++i) 
+      {
+        // add a path not exceeding <steps> binary values to candidate using bit shiftting.
+        size_t bit = (MyBit >> i) & 1; 
+        if (bit == 1) 
+        {
+          if (candidate.is_step_valid(STEP_DIRECTION_EAST)) 
+          {
+            candidate.add_step(STEP_DIRECTION_EAST);
+          }
+          else status = false;
+        }
+        else 
+        {
+          if (candidate.is_step_valid(STEP_DIRECTION_SOUTH)) 
+          {
+            candidate.add_step(STEP_DIRECTION_SOUTH);
+          }
+          else status = false;
+        }  
+      }
+      if (status && (candidate.total_cranes() > best.total_cranes())) 
+      {
+        best = candidate;
+      }
+    }
+  }
+  return best;
 }
+
+
+
+
+
+
+
+
+
 
 // Solve the crane unloading problem for the given grid, using a dynamic
 // programming algorithm.
 //
 // The grid must be non-empty.
-//path crane_unloading_dyn_prog(const grid& setting) {
-path crane_unloading_dyn_prog(const grid& setting) {
-
+path crane_unloading_dyn_prog(const grid& setting) 
+{
   // grid must be non-empty.
   assert(setting.rows() > 0);
   assert(setting.columns() > 0);
-
-  
   using cell_type = std::optional<path>;
-
-  std::vector<std::vector<cell_type> > A(setting.rows(),
-                                        std::vector<cell_type>(setting.columns()));
-
+  std::vector<std::vector<cell_type>> A(setting.rows(), std::vector<cell_type>(setting.columns()));
   A[0][0] = path(setting);
   assert(A[0][0].has_value());
+  for (coordinate row = 0; row < setting.rows(); ++row) 
+  {
+    for (coordinate col = 0; col < setting.columns(); ++col) 
+    {
+      if (setting.get(row, col) != CELL_BUILDING) 
+      {
+        // set the value for A[row][col] as a path collecting most cranes
+        // continue;
+        cell_type Overhead;
+        cell_type At_West;
 
-  for (coordinate r = 0; r < setting.rows(); ++r) {
-    for (coordinate c = 0; c < setting.columns(); ++c) {
-
-      if (setting.get(r, c) == CELL_BUILDING){
-        A[r][c].reset();
-        continue;
+        if (row > 0 && A[row-1][col].has_value()) 
+        {
+          Overhead = A[row-1][col];
+          if (Overhead->is_step_valid(STEP_DIRECTION_SOUTH)) 
+          {
+            Overhead->add_step(STEP_DIRECTION_SOUTH);
+          }
         }
-
-    cell_type from_above = std::nullopt;
-    cell_type from_left = std::nullopt;
-
-	    // TODO: implement the dynamic programming algorithm, then delete this
-  // comment.
-
-   assert(best->has_value());
-//  //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
-
-   return **best;
-	}
-
+        if (col > 0 && A[row][col-1].has_value()) 
+        {
+          At_West = A[row][col-1];
+          if (At_West->is_step_valid(STEP_DIRECTION_EAST)) 
+          {
+            At_West->add_step(STEP_DIRECTION_EAST);
+          }
+        }
+        // whichever of from_above and At_West is non-None and reaches more cranes; 
+        // or None if both from_above and At_West are None
+        if (Overhead.has_value() && At_West.has_value()) 
+        {
+          if (Overhead->total_cranes() > At_West->total_cranes()) 
+          {
+            A[row][col] = Overhead;
+          }
+          else 
+          {
+            A[row][col] = At_West;
+          }
+        }
+        if (Overhead.has_value() && !(At_West.has_value())) 
+        {
+          A[row][col] = Overhead;
+        }
+        if (At_West.has_value() && !(Overhead.has_value())) 
+        {
+          A[row][col] = At_West;
+        }
+      }
+    }
+  }      
+  cell_type* best = &(A[0][0]);
+  assert(best->has_value());
+  for (coordinate row = 0; row < setting.rows(); ++row) 
+  {
+    for (coordinate col = 0; col < setting.columns(); ++col) 
+    {
+      if (A[row][col].has_value() && A[row][col]->total_cranes() > (*best)->total_cranes()) 
+      {
+        best = &(A[row][col]);
+      }
+    }
+  }
+  assert(best->has_value());
+  return **best;
+}
 }
